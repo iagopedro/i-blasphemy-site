3583e7450034dd472d81f7b7764c06fae1f508bf
iagopedro <iagopedroltd19@gmail.com>
2021-04-26 10:33:45 -0300

    fixed wrong links that came with the pull

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -19,20 +19,20 @@ function Main() {
       <li className='list-category'>Prés</li>
         <Item 
           name={'Extinção'}
-          link={'https://drive.google.com/file/d/1nvrI3KMZ_oL6tiOsY6m3z8FkqIZxKgeR/view?usp=sharing'}
+          link={'https://drive.google.com/file/d/11dsiJlwPvIq4i-cU0GKDp-CBaG7d-E1u/view?usp=sharing'}
         />
         <Item 
           name={'Vai Dar Certo'}
-          link={'https://drive.google.com/file/d/1nvrI3KMZ_oL6tiOsY6m3z8FkqIZxKgeR/view?usp=sharing'}
+          link={'https://drive.google.com/file/d/1vAT6QXreQ2dmiN0mIzTUmoC2oTVT_J9X/view?usp=sharing'}
         />
         <Item 
           name={'Legado'}
-          link={'https://drive.google.com/file/d/1nvrI3KMZ_oL6tiOsY6m3z8FkqIZxKgeR/view?usp=sharing'}
+          link={'https://drive.google.com/file/d/1Gk7dVl1hnAlEwrRPQcNZuF_wEiM2Rij7/view?usp=sharing'}
         />
       <li className='list-category'>Fotos</li>
         <Item 
-          name={'Ensaio fotográfico no Monte dos Guararapes'}
-          link={'https://drive.google.com/drive/folders/1kqYF44TVssrCY1J1uWXefCijA7alfMde?usp=sharing'}
+          name={'Ensaio - Monte dos Guararapes'}
+          link={'https://drive.google.com/drive/folders/1WZdwJbE8ILf2-zjOEp9xokklahebGQJh?usp=sharing'}
         />
         <Item 
           name={'Ensaio - 21/06/2020'}
@@ -41,7 +41,7 @@ function Main() {
       <li className='list-category'>Documentos</li>
         <Item 
           name={'Release'}
-          link={'https://drive.google.com/file/d/1fCyWo3n2NdtiBk9acQU8AfOSG1Q1FRLi/view?usp=sharinghttps://drive.google.com/file/d/1fCyWo3n2NdtiBk9acQU8AfOSG1Q1FRLi/view?usp=sharing'}
+          link={'https://docs.google.com/document/d/1KROG8InETJx6_bltV36PNKvpjZs4Tef1GyhJKNfEvBg/edit?usp=sharing'}
         />
       <li className='list-category'>Letras</li>
        <Item 
@@ -50,7 +50,7 @@ function Main() {
        />
        <Item 
         name={'Desespero'}
-        link={'https://drive.google.com/file/d/1Cz53BDep1vHdErpf_wSXXi0NkqfbQeQ3/view?usp=sharing'}
+        link={'https://drive.google.com/file/d/1CLuEDcG4AoqINpE1D_r7Un1Xf05qwNc2/view?usp=sharing'}
        />
        <Item 
         name={'Extinção'}
